be6a24ce1f9133106680f994abdf9aeca9dbdb3d
Sam Edwardes <edwardes.s@gmail.com>
2022-01-10 09:14:54 -0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -17,8 +17,8 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 # Install pyenv
 sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl https://pyenv.run | bash
-# pyenv install 3.7.12
-# pyenv install 3.8.12
+pyenv install 3.7.12
+pyenv install 3.8.12
 pyenv install 3.9.9
 pyenv install 3.10.1
 
@@ -29,7 +29,6 @@ python -m pip install --upgrade pip
 pip install wheel
 pip install pipx
 
-
 # Create default python virtual environments
 mkdir ~/.virtualenvs
 python -m venv ~/.virtualenvs/py310
@@ -37,4 +36,7 @@ source ~/.virtualenvs/py310/bin/activate
 python -m pip install --upgrade pip
 pip install wheel
 pip install jupyterlab pandas dash streamlit
+
+# Set up poetry
+curl -sSL https://install.python-poetry.org | python3 -
 
